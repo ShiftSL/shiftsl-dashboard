0374fe49ce06ee10e3b147a694168d4a4b13038b
NilasiJayathilake <136611590+NilasiJayathilake@users.noreply.github.com>
2025-02-19 12:07:32 +0530

    custom time slots were updated

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -10,9 +10,10 @@ import Calendar from "../Components/Calendar.tsx"; // Import Navbar
 const Dashboard: React.FC = () => {
     return (
         <>
-            <Navbar /> {/* Use the Navbar component here */}
+            {/*<Navbar /> /!* Use the Navbar component here *!/*/}
             <Calendar/>
-
+        {/*    TODO: Ojitha = When the NavBar is open the Time Axis is hidden Fix that
+                               Make the Page Dynamic Also Fix nav Bar when Collapsed*/}
         </>
     );
 };
