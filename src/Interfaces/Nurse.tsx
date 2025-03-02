4040d302a1699d5bf8dc6b496c8dbe7f93cb2334
hasindie <hasindie@gmail.com>
2025-03-02 14:13:33 +0530

    Initialize nurse roster files

---
+++ src/Interfaces/Nurse.tsx
@@ -0,0 +1,8 @@
+export interface Nurse {
+    id: bigint;
+    first_name: string;
+    last_name: string;
+    email: string;
+    role: string; // check the role cause the backend is designed to understand whether it's a doctor, or an admin
+    phone_no: string;
+}
